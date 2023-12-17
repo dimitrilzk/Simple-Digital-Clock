256b6698a00f2d3a50500fb4b35894aff064dbcd
dimitrilzk <dimitrilazuka95@gmail.com>
2023-12-17 22:53:35 +0100

    orologio funzionante

---
+++ wwwroot/js/clock.js
@@ -0,0 +1,18 @@
+﻿function showTime() {
+    let time = new Date();
+    let hour = time.getHours();
+    let minute = time.getMinutes();
+    let second = time.getSeconds();
+
+    if (second < 10) {
+        second = "0" + second;
+    }
+
+    let currentTime = hour + ":" + minute + ":" + second;
+    $("#clock").html(currentTime);
+    console.log(currentTime);
+}
+
+setInterval(showTime, 1000);
+
+showTime();
